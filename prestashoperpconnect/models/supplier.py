0aa2785a7caabe9ec2f6a2fa51ac2e27b365f747
florent.thomas <florent.thomas@mind-and-go.com>
2015-09-09 13:51:27 +0200

    Add licence
    Add licence

--- prestashoperpconnect/models/supplier.py
+++ prestashoperpconnect/models/supplier.py
@@ -1,8 +1,32 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#   Module for OpenERP
+#   Copyright (C) 2014 Akretion (http://www.akretion.com).
+#   @author Sébastien BEAU <sebastien.beau@akretion.com>
+#   @author Guewen Baconnier
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+
 from openerp.osv import fields, orm
 
 from openerp.addons.connector.session import ConnectorSession
 
-from ..unit.import_synchronizer import import_record
+from openerp.addons.prestashoperpconnect.unit.import_synchronizer import import_record
 
 
 class res_partner(orm.Model):
