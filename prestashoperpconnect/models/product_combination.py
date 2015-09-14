b10cc703c6613caff4b1177338eab7bdaae112cd
florent.thomas <florent.thomas@mind-and-go.com>
2015-09-14 12:44:14 +0200

    Add licence

--- prestashoperpconnect/models/product_combination.py
+++ prestashoperpconnect/models/product_combination.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Prestashoperpconnect : OpenERP-PrestaShop connector
+#    Copyright (C) 2013 Akretion (http://www.akretion.com/)
+#    @author: Alexis de Lattre <alexis.delattre@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 '''
 A product combination is a product with different attributes in prestashop.
 In prestashop, we can sell a product or a combination of a product with some
@@ -14,7 +36,7 @@ from openerp.osv import fields, orm
 
 from openerp.addons.connector.session import ConnectorSession
 
-from ..unit.import_synchronizer import import_record
+from openerp.addons.prestashoperpconnect.unit.import_synchronizer import import_record
 
 
 class product_product(orm.Model):
