664b6ccd7f1a5845725f66d4887a747ef805a06b
Florent THOMAS <florent.thomas@mind-and-go.com>
2016-10-02 17:32:03 +0200

    Bind Taxes if configuration and name are the same

--- connector_prestashop/models/account_tax/importer.py
+++ connector_prestashop/models/account_tax/importer.py
@@ -3,6 +3,7 @@
 
 
 from ...unit.auto_matching_importer import AutoMatchingImporter
+from openerp.addons.connector.unit.mapper import only_create, mapping
 from ...backend import prestashop
 
 
@@ -23,3 +24,18 @@ class AccountTaxImporter(AutoMatchingImporter):
                 erp_dict['amount_type'] == 'percent' and
                 abs(erp_val - float(ps_val)) < 0.01 and
                 self.backend_record.company_id.id == erp_dict['company_id'][0])
+
+
+    @only_create
+    @mapping
+    def openerp_id(self, record):
+        """ Will bind the tax to an existing one with the same name and conf """
+        tax = self.env['account.tax'].search([
+                        ('name', '=', self.name(record)['name']),
+                        ('company_id', '=', self.company_id(record)['company_id']),
+                        ('amount', '=', self.amount(record)['amount']),
+                        ('price_include', '=', self.price_include(record)['price_include']),
+        ]   
+        , limit=1)
+        if tax:
+            return {'openerp_id': tax.id}
