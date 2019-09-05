6be6dc1bdafb89dc9c4a121ef8e50ed9dc39d9f6
Quentin THEURET <quentin@quentin-theuret.net>
2019-09-05 09:11:13 +0200

    [FIX] FIxed missing import

--- connector_prestashop/models/res_partner/common.py
+++ connector_prestashop/models/res_partner/common.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 from odoo.addons.component.core import Component
 from odoo.addons.queue_job.job import job
